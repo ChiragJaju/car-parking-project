1495253a0e457fb0009eecc07615022e6a3a4880
DarkLord0206 <tulsyan.parth0206@gmail.com>
2021-11-16 16:48:04 +0530

    uploaded backend

---
+++ java/src/main/java/springboot/User.java
@@ -0,0 +1,20 @@
+package springboot;
+
+import java.util.HashMap;
+
+public class User
+{
+
+    private String l_name,email,mobile,u_name,pwd;
+    private HashMap<String,String> login = new HashMap<>();
+    private double balance;
+    private Address Addrs;
+    private Car car_data;
+
+
+//IMPLEMENT EQUALS FUNCTION based on email,username
+}
+
+
+
+
---
+++ java/src/main/java/springboot/Worker.java
@@ -0,0 +1,7 @@
+package springboot;
+
+public class Worker
+{
+    int rating;
+    String name;
+}
