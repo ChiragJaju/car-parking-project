388bd483f3126dfe32cbf7f91bed1b7c05ec3b76
itachi2204 <devarshi224@gmail.com>
2021-11-18 03:29:31 +0530

    already exist

--- src/main/java/springboot/Building.java
+++ src/main/java/springboot/Building.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 import springboot.*;
 public class Building
 {
-    ArrayList<User> users = new ArrayList<>();
+    static ArrayList<User> users = new ArrayList<>();
     ArrayList<Car> cars = new ArrayList<>();
     ArrayList<User> waitingList = new ArrayList<>();
     private final int charge = 25;
@@ -13,7 +13,27 @@ public class Building
     ArrayList<Worker> workers = new ArrayList<>();
     ArrayList<ParkingLot> parkingLot = new ArrayList<>();
 
-    public boolean canAdd(User user) {
-        return true && true;
+    // public boolean canAdd(User user) 
+    // {
+    //     return true && true;
+    // }
+    public static boolean already_exists(User x)
+    {
+        for(int i =0;i<users.size();i++)
+        {
+            if(users.get(i).u_name == x.u_name)
+            {
+                return false;
+            }
+            if(users.get(i).email == x.email)
+            {
+                return false;
+            }
+            if(users.get(i).mobile == x.mobile)
+            {
+                return false;
+            }
+        }
+        return true;
     }
 }
